bd410dc2406638b24ef0fa2e2a081cecd53ace4c
DESKTOP-USHSCCC\csi-r <csi22-rcoriaz@colegioaltair.net>
2023-01-30 09:20:33 +0100

    no message

---
+++ edu.javaScript.charAtEj/src/main/webapp/script/charAt.js
@@ -0,0 +1,6 @@
+function ejemplo() {
+	let palabra = "hola";
+	let primeraLetra = palabra.charAt(0);
+	let ultimaLetra = palabra.charAt(palabra.length - 1);
+	document.write(palabra + "<br>" + "Primera letra: \n" + primeraLetra + "<br>Última letra: \n" + ultimaLetra);
+}
